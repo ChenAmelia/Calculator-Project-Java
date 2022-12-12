01b2129043a1713fc8be815235fa5d570c332016
Chen <liangchen_amelia@163.com>
2022-12-12 18:57:52 +0000

    Strat multi calculation

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -38,8 +38,43 @@ public class Calculator {
                     break;
                 }
         }
+    }
+
+    public void multiCalculation(String oper) {
+
+        double num1 = Double.parseDouble(String.valueOf(oper.charAt(0)));
+        double num2 = Double.parseDouble(String.valueOf(oper.charAt(2)));
+        double num3 = Double.parseDouble(String.valueOf(oper.charAt(4)));
+
+        String cal1 = String.valueOf(oper.charAt(1));
+        String cal2 = String.valueOf(oper.charAt(3));
+
+        double result;
+        double result1;
 
 
+
+
+
+
+//        if(cal1.equals("*") || cal1.equals("/")) {
+//            switch(cal1){
+//                case "*":
+//                    result1 = num1 * num2;
+//                    System.out.println(num1 + " * " + num2 + " = " + result1);
+//                    break;
+//                case "/":
+//                    if(num2 == 0){
+//                        System.out.println("The Division Number Can Not Be 0.");
+//                    }else{
+//                        result1 = num1 / num2;
+//                        System.out.println(num1 + " / " + num2 + " = " + result1);
+//                    }
+//
+//            }
+//
+//
+//        }
 
 
 
